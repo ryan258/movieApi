02e44ed72e70cc212c7ff5e8c94617eccc541823
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-08-05 13:49:33 -0500

    Add basic routes to start

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet')
 
+// /now_playing
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// /movies/...
+const movieRouter = require('./routes/movie');
+// /search/...
+const searchRouter = require('./routes/search');
 
 var app = express();
+app.use(helmet())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +25,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//! Since we're making an API, routes are going to be very important for this app.
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/movie', movieRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
